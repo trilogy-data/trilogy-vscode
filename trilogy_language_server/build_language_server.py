b18e7c5b7de60e5c8397caad38790c19736362cf
efromvt <ethan.dickinson@gmail.com>
2024-09-16 14:06:46 -0400

    fix linux execution

--- trilogy_language_server/build_language_server.py
+++ trilogy_language_server/build_language_server.py
@@ -19,7 +19,7 @@ if system() == "Linux":
     final_file = SCRIPT_NAME
 elif system() == "Windows":
     parent = "scripts"
-    final_file = f"{SCRIPT_NAME}"
+    final_file = f"{SCRIPT_NAME}.exe"
 else:
     parent = "bin"
     final_file = SCRIPT_NAME
